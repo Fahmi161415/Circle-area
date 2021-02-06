8d904d25f89d302a12fb66972cd6f54c5269f7d5
Fahmi161415 <77283001+Fahmi161415@users.noreply.github.com>
2021-02-06 19:01:32 +0530

    Created areacircle.py
    For getting area of any radius value

---
+++ areacircle.py
@@ -0,0 +1,7 @@
+# For calculating area of circle
+
+
+radius = eval(input("Enter the value of radius: "))
+area=3.1415922653589793238*radius*radius
+print("Area of circle having radius =", radius,"is", area)
+
